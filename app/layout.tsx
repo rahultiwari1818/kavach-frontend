2861d9cd87512a9d6326fcf7d3341dc5ac9d18c3
lucky1218 <rahultiwari161189@gmail.com>
2025-09-29 13:53:00 +0530

    setup basic login page in app router and layout with navbar-- also created navbar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ToastProvider from "@/src/context/ToastProvider";
+import NavbarWrapper from "@/src/components/Navbar/NavbarWrapper";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import 'leaflet/dist/leaflet.css';
 
 
 const geistSans = Geist({
@@ -28,7 +32,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-
+        <ToastProvider>
+          <GoogleOAuthProvider
+            clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}
+          >
+            <NavbarWrapper />
+            {children}
+          </GoogleOAuthProvider>
+        </ToastProvider>
       </body>
     </html>
   );
